dce05ed3f5125bcf6da9c993b019fc6a695b182a
Daniel Paschke <paschdan@gmail.com>
2017-06-21 12:34:43 +0200

    add react-redux
    connect App to react-redux

--- client/index.js
+++ client/index.js
@@ -1,5 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App.jsx';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import App from './components/App.jsx';
+import reducer from './reducers/index.js'
+
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+)
+ReactDOM.render(
+  <Provider store={store} >
+    <App />
+  </Provider>,
+  document.getElementById('root'));
