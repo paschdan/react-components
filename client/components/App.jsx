dbbca352655ca0e80d9bcb6ba8f32fed1e74e4ee
Daniel Paschke <paschdan@gmail.com>
2017-06-21 13:12:17 +0200

    Use the smart component
    * remove greetTo, as it is ignored by the smart component
    * rename component to visualize that it is smart now

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import Greetings from './greetings.jsx'
+import GreetingsContainer from './greetings.jsx'
 
 const App = () =>
   (
     <div style={{ textAlign: 'center' }}>
-      <Greetings greetWord="Hello" greetTo="Split"/>
+      <GreetingsContainer greetWord="Hello" />
     </div>
   );
 
